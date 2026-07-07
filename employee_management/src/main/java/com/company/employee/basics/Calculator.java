8f4efe52b6a6d5a2906d9a921c89a44ea526e088
dude-here <63449722+dude-here@users.noreply.github.com>
2026-07-07 21:17:22 +0530

    Update Calculator.java
    added Modulus method

--- employee_management/src/main/java/com/company/employee/basics/Calculator.java
+++ employee_management/src/main/java/com/company/employee/basics/Calculator.java
@@ -9,7 +9,12 @@ public class Calculator {
 	public int subtract(int a, int b) {
 		return a - b;
 	}
-
+	
+	//this method added in Github(Origin) main and committed in  Github/Origin
+	public int modulus(int a, int b) {
+		return a % b;
+	}
+	
 	// created via multiplication branch
 	public int multiply(int a, int b) {
 
@@ -30,6 +35,7 @@ public class Calculator {
 		System.out.println("Subtraction : " + calculator.subtract(20, 10));
 		System.out.println("Multiplication is : " + calculator.multiply(20, 10));
 		System.out.println("Division is : " + calculator.division(20, 10));
+		System.out.println("Division is : " + calculator.modulus(30, 10));//also added in Origin Main
 
 	}
 }
