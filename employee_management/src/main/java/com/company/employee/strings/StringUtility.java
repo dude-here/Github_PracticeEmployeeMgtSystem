8274376b5916aebce261bba5139e8ea3c1f8cb2c
dude-here <jaydevrathod125@gmail.com>
2026-07-08 17:21:22 +0530

    Numeric method added via same Local StringOperationFeature after first successfull PR

--- employee_management/src/main/java/com/company/employee/strings/StringUtility.java
+++ employee_management/src/main/java/com/company/employee/strings/StringUtility.java
@@ -7,41 +7,44 @@ public class StringUtility {
 		return new StringBuilder(text).reverse().toString();
 	}
 
-	
-	// 1. Convert to Uppercase and ADDED via String branch 
-    public String toUpper(String text) {
-        return text.toUpperCase();
-    }
-	
-    // 2. Concatenate Strings and ADDED via String branch
-    public String concatenate(String str1, String str2) {
-        return str1.concat(str2);
-    }
-    
-    
-    //3. Remove First Character and Added via 2nd Commit via String branch
-    public String removeFirstCharacter(String text) {
-        return text.substring(1);
-    }
-    
-	//second Bug Resolved in the BugFixBranch
+	// 1. Convert to Uppercase and ADDED via String branch
+	public String toUpper(String text) {
+		return text.toUpperCase();
+	}
+
+	// 2. Concatenate Strings and ADDED via String branch
+	public String concatenate(String str1, String str2) {
+		return str1.concat(str2);
+	}
+
+	// 3. Remove First Character and Added via 2nd Commit via String branch
+	public String removeFirstCharacter(String text) {
+		return text.substring(1);
+	}
+
+	// second Bug Resolved in the BugFixBranch
 	public int countCharacters(String text) {
 
 		return text.length();
 	}
-	
-	// 4. Remove Last Character 
-	//Fixed This Bug in BugFixBranch
-    public String removeLastCharacter(String text) {
-        return text.substring(0, text.length() - 1);
-    }
-    
-    
-    //5. Check Palindrome String and ADDED FEature via Local StringOperationFeature branch 
-    public boolean isPalindrome(String text) {
-        return text.equalsIgnoreCase(new StringBuilder(text).reverse().toString());
-    }
 
+	// 4. Remove Last Character
+	// Fixed This Bug in BugFixBranch
+	public String removeLastCharacter(String text) {
+		return text.substring(0, text.length() - 1);
+	}
+
+	// 5. Check Palindrome String and ADDED FEature via Local StringOperationFeature
+	// branch
+	public boolean isPalindrome(String text) {
+		return text.equalsIgnoreCase(new StringBuilder(text).reverse().toString());
+	}
+
+	// 6. Check Numeric method added via same Local StringOperationFeature after 
+	//first successfull PR
+	public boolean isNumeric(String text) {
+		return text.matches("\\d+");
+	}
 
 	public static void main(String[] args) {
 
@@ -49,12 +52,15 @@ public class StringUtility {
 
 		System.out.println("Reverse : " + utility.reverse("GitHub"));
 		System.out.println("Length : " + utility.countCharacters("GitHub"));
-		System.out.println("Uppercase : "+utility.toUpper("jaydev"));
-		System.out.println("Concatenate : "+utility.concatenate("jaydev","rathod"));
-		System.out.println("Remove First Char : "+utility.removeFirstCharacter("rathod"));
-		System.out.println("Remove Last Char : "+utility.removeLastCharacter("tanmay"));
+		System.out.println("Uppercase : " + utility.toUpper("jaydev"));
+		System.out.println("Concatenate : " + utility.concatenate("jaydev", "rathod"));
+		System.out.println("Remove First Char : " + utility.removeFirstCharacter("rathod"));
+		System.out.println("Remove Last Char : " + utility.removeLastCharacter("tanmay"));
+
+		System.out.println("Palindrome cHeck : " + utility.isPalindrome("MOM"));
+		System.out.println("Is Numeric : " + utility.isNumeric("jay"));
 		
-		System.out.println("Palindrome cHeck : "+utility.isPalindrome("MOM"));
 		
+
 	}
 }
